85567d1c97cf36b57a190a2bf5c40f16c6fc61c0
huzhihang <huzhihang32@gmail.com>
2020-01-21 17:59:29 +0800

    update

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,7 @@
 /** @format */
 import React from 'react';
-import { Card } from '@material-ui/core';
+import { Card, Grid } from '@material-ui/core';
+import { Cat } from 'react-kawaii';
 import { makeStyles } from '@material-ui/core/styles';
 import trainIcon from '../../assets/train.png';
 import searchIcon from '../../assets/search.png';
@@ -10,7 +11,7 @@ import vpnIcon from '../../assets/vpn.png';
 
 const useStyles = makeStyles({
     card: {
-        padding: 8,
+        padding: 3,
         cursor: 'pointer',
         display: 'flex',
         justifyContent: 'center',
@@ -18,13 +19,6 @@ const useStyles = makeStyles({
     },
     icon: {
         width: 90,
-        height: 90,
-    },
-    container: {
-        display: 'flex',
-    },
-    cardBox: {
-        padding: 30,
     },
     a: {
         textDecoration: 'none',
@@ -32,51 +26,68 @@ const useStyles = makeStyles({
         color: '#666',
         lineHeight: 2,
     },
+    header: {
+        width: '100%',
+        height: 150,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        background: 'rgb(205, 236, 254)',
+        color: '#555',
+    },
+    content: {
+        padding: 20,
+    },
 });
+
+const apps = [
+    {
+        title: '12306刷票',
+        href: 'http://12306.huzhihang.site:8081/',
+        icon: trainIcon,
+    },
+    {
+        title: 'bt搜索',
+        href: 'http://bt.huzhihang.site:8081/',
+        icon: searchIcon,
+    },
+    {
+        title: '监控系统',
+        href: 'http://monitor.huzhihang.site:8081/',
+        icon: shieldIcon,
+    },
+    {
+        title: 'blog',
+        href: 'http://blog.huzhihang.site/',
+        icon: articleIcon,
+    },
+    {
+        title: '紧急翻墙',
+        href: 'https://orange-sky-6799.cross.workers.dev/',
+        icon: vpnIcon,
+    },
+];
 
 export default function Home() {
     const classes = useStyles();
     return (
-        <div className={classes.container}>
-            <div className={classes.cardBox}>
-                <a className={classes.a} href="http://12306.huzhihang.site:8081/" target="__blank">
-                    <Card className={classes.card}>
-                        <img src={trainIcon} className={classes.icon} />
-                    </Card>
-                    <div>12306刷票</div>
-                </a>
+        <div>
+            <div className={classes.header}>
+                <Cat size={100} mood="happy" color="#596881" />
             </div>
-            <div className={classes.cardBox}>
-                <a className={classes.a} href="http://bt.huzhihang.site:8081/" target="__blank">
-                    <Card className={classes.card}>
-                        <img src={searchIcon} className={classes.icon} />
-                    </Card>
-                    <div>bt搜索</div>
-                </a>
-            </div>
-            <div className={classes.cardBox}>
-                <a className={classes.a} href="http://monitor.huzhihang.site:8081/" target="__blank">
-                    <Card className={classes.card}>
-                        <img src={shieldIcon} className={classes.icon} />
-                    </Card>
-                    <div>监控系统</div>
-                </a>
-            </div>
-            <div className={classes.cardBox}>
-                <a className={classes.a} href="http://blog.huzhihang.site/" target="__blank">
-                    <Card className={classes.card}>
-                        <img src={articleIcon} className={classes.icon} />
-                    </Card>
-                    <div>blog</div>
-                </a>
-            </div>
-            <div className={classes.cardBox}>
-                <a className={classes.a} href="https://orange-sky-6799.cross.workers.dev/" target="__blank">
-                    <Card className={classes.card}>
-                        <img src={vpnIcon} className={classes.icon} />
-                    </Card>
-                    <div>紧急翻墙</div>
-                </a>
+            <div className={classes.content}>
+                <Grid container spacing={3}>
+                    {apps.map(app => (
+                        <Grid item xs={4} lg={1} key={app.title}>
+                            <a className={classes.a} href={app.href} target="__blank">
+                                <Card className={classes.card}>
+                                    <img src={app.icon} className={classes.icon} />
+                                </Card>
+                                <div>{app.title}</div>
+                            </a>
+                        </Grid>
+                    ))}
+                </Grid>
             </div>
         </div>
     );
